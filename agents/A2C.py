hyperparameters and computation checked. A2C baseline fixed

agents/A2C.py
@@ -14,44 +14,30 @@ from timeit import default_timer as timer
 class Model(BaseAgent):
     def __init__(self, static_policy=False, env=None, config=None, log_dir='/tmp/gym'):
         super(Model, self).__init__(config=config, env=env, log_dir=log_dir)
-        self.device = config.device
-        self.conv_out = config.conv_out
-
-        self.recurrent_policy = config.recurrent_policy_grad
-        self.gru_size = config.gru_size
-
-        self.gamma = config.GAMMA
-        self.lr = config.LR
-        self.num_agents = config.num_agents
-        self.value_loss_weight = config.value_loss_weight
-        self.entropy_loss_weight = config.entropy_loss_weight
-        self.rollout = config.rollout
-        self.grad_norm_max = config.grad_norm_max
-
+        self.config = config
         self.static_policy = static_policy
         self.num_feats = env.observation_space.shape
-        self.num_feats = (self.num_feats[0], *self.num_feats[1:])
         self.num_actions = env.action_space.n
         self.env = env
 
         self.declare_networks()
             
-        if not self.recurrent_policy:
-            self.optimizer = optim.RMSprop(self.model.parameters(), lr=self.lr, alpha=0.99, eps=1e-5)
+        if not self.config.recurrent_policy_grad:
+            self.optimizer = optim.RMSprop(self.model.parameters(), lr=self.config.LR, alpha=0.99, eps=1e-5)
         else:
-            self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, amsgrad=True)
+            self.optimizer = optim.Adam(self.model.parameters(), lr=self.config.LR, amsgrad=True)
         
         #move to correct device
-        self.model = self.model.to(self.device)
+        self.model = self.model.to(self.config.device)
 
         if self.static_policy:
             self.model.eval()
         else:
             self.model.train()
 
-        self.rollouts = RolloutStorage(self.rollout, self.num_agents,
+        self.config.rollouts = RolloutStorage(self.config.rollout, self.config.num_agents,
             self.num_feats, self.env.action_space, self.model.state_size,
-            self.device, config.USE_GAE, config.gae_tau)
+            self.config.device, config.USE_GAE, config.gae_tau)
 
         self.value_losses = []
         self.entropy_losses = []
@@ -59,7 +45,7 @@ class Model(BaseAgent):
 
 
     def declare_networks(self):
-        self.model = ActorCriticAtari(self.num_feats, self.num_actions, self.conv_out, self.recurrent_policy, self.gru_size)
+        self.model = ActorCriticAtari(self.num_feats, self.num_actions, self.config.recurrent_policy_grad, self.config.gru_size)
         
 
     def get_action(self, s, states, masks, deterministic=False):
@@ -93,10 +79,12 @@ class Model(BaseAgent):
 
         return values
 
-    def compute_loss(self, rollouts):
+    def compute_loss(self, rollouts, next_value):
         obs_shape = rollouts.observations.size()[2:]
         action_shape = rollouts.actions.size()[-1]
         num_steps, num_processes, _ = rollouts.rewards.size()
+
+        self.config.rollouts.compute_returns(next_value, self.config.GAMMA)
 
         values, action_log_probs, dist_entropy, states = self.evaluate_actions(
             rollouts.observations[:-1].view(-1, *obs_shape),
@@ -112,22 +100,25 @@ class Model(BaseAgent):
 
         action_loss = -(advantages.detach() * action_log_probs).mean()
 
-        loss = action_loss + self.value_loss_weight * value_loss
-        loss -= self.entropy_loss_weight * dist_entropy
+        loss = action_loss + self.config.value_loss_weight * value_loss
+        loss -= self.config.entropy_loss_weight * dist_entropy
 
         return loss, action_loss, value_loss, dist_entropy
 
-    def update(self, rollout):
-        loss, action_loss, value_loss, dist_entropy = self.compute_loss(rollout)
+    def update(self, rollout, next_value):
+        loss, action_loss, value_loss, dist_entropy = self.compute_loss(rollout, next_value)
 
         self.optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_norm_max)
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.grad_norm_max)
         self.optimizer.step()
 
         #self.save_loss(loss.item(), action_loss.item(), value_loss.item(), dist_entropy.item())
 
         return value_loss.item(), action_loss.item(), dist_entropy.item()
+
+    def save_distance(self, max_dist, tstep):
+        pass
 
     '''def save_loss(self, loss, policy_loss, value_loss, entropy_loss):
         super(Model, self).save_loss(loss)

agents/ICM_A2C.py
@@ -6,14 +6,14 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 
-from agents.BaseAgent import BaseAgent
+from agents.A2C import Model as A2C_Model
 from networks.networks import ActorCriticSMB
 from networks.special_units import IC_Features, IC_ForwardModel_Head, IC_InverseModel_Head
 from utils.RolloutStorage import RolloutStorage
 
 from timeit import default_timer as timer
 
-class Model(BaseAgent):
+class Model(A2C_Model):
     def __init__(self, static_policy=False, env=None, config=None, log_dir='/tmp/gym'):
         super(Model, self).__init__(config=config, env=env, log_dir=log_dir)
         self.config = config
@@ -64,32 +64,6 @@ class Model(BaseAgent):
         self.ICMBackwardModel = IC_InverseModel_Head(self.ICMfeaturizer.feature_size()*2, self.num_actions)
         
 
-    def get_action(self, s, states, masks, deterministic=False):
-        logits, values, states = self.model(s, states, masks)
-        dist = torch.distributions.Categorical(logits=logits)
-
-        if deterministic:
-            actions = dist.probs.argmax(dim=1, keepdim=True)
-        else:
-            actions = dist.sample().view(-1, 1)
-
-        log_probs = F.log_softmax(logits, dim=1)
-        action_log_probs = log_probs.gather(1, actions)
-
-        return values, actions, action_log_probs, states
-
-    def evaluate_actions(self, s, actions, states, masks):
-        logits, values, states = self.model(s, states, masks)
-
-        dist = torch.distributions.Categorical(logits=logits)
-
-        log_probs = F.log_softmax(logits, dim=1)
-        action_log_probs = log_probs.gather(1, actions)
-
-        dist_entropy = dist.entropy().mean()
-
-        return values, action_log_probs, dist_entropy, states
-
     def icm_get_features(self, s):
         return self.ICMfeaturizer(s)
 
@@ -104,13 +78,7 @@ class Model(BaseAgent):
 
         return logits
 
-
-    def get_values(self, s, states, masks):
-        _, values, _ = self.model(s, states, masks)
-
-        return values
-
-    def compute_loss(self, rollouts, next_value=None):
+    def compute_loss(self, rollouts, next_value):
         obs_shape = rollouts.observations.size()[2:]
         action_shape = rollouts.actions.size()[-1]
         num_steps, num_processes, _ = rollouts.rewards.size()
@@ -123,13 +91,13 @@ class Model(BaseAgent):
         obs_diff = icm_obs_pred - phi[self.config.num_agents:]
 
         #add intrinsic reward
-        if next_value is not None:
-            with torch.no_grad():
-                intr_reward = obs_diff.pow(2).sqrt().sum(dim=1)
-                intr_reward = intr_reward.view(num_steps, num_processes, 1)
-            self.config.rollouts.rewards += intr_reward.detach()
-            self.config.rollouts.rewards = torch.clamp(self.config.rollouts.rewards, min=-1.0, max=1.0)
-            self.config.rollouts.compute_returns(next_value, self.config.GAMMA)
+        with torch.no_grad():
+            intr_reward = obs_diff.pow(2).sqrt().sum(dim=1)
+            intr_reward = self.config.icm_prediction_beta * intr_reward.view(num_steps, num_processes, 1)
+        self.config.rollouts.rewards += intr_reward.detach()
+        self.config.rollouts.rewards = torch.clamp(self.config.rollouts.rewards, min=-1.0, max=1.0)
+        
+        self.config.rollouts.compute_returns(next_value, self.config.GAMMA)
 
         values, action_log_probs, dist_entropy, states = self.evaluate_actions(
             rollouts.observations[:-1].view(-1, *obs_shape),
@@ -154,33 +122,14 @@ class Model(BaseAgent):
         loss = action_loss + self.config.value_loss_weight * value_loss
         loss *= self.config.icm_lambda
 
-        loss-= ((1.-self.config.icm_beta)*inverse_model_loss)
-        loss-= (self.config.icm_beta*forward_model_loss)
+        loss-= ((1.-self.config.icm_loss_beta)*inverse_model_loss)
+        loss-= (self.config.icm_loss_beta*forward_model_loss)
 
         loss -= self.config.entropy_loss_weight * dist_entropy
 
         return loss, action_loss, value_loss, dist_entropy
 
-    def update(self, rollout, next_value=None):
-        loss, action_loss, value_loss, dist_entropy = self.compute_loss(rollout, next_value)
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.grad_norm_max)
-        self.optimizer.step()
-
-        #self.save_loss(loss.item(), action_loss.item(), value_loss.item(), dist_entropy.item())
-
-        return value_loss.item(), action_loss.item(), dist_entropy.item()
-
     def save_distance(self, max_dist, tstep):
         with open(os.path.join(self.log_dir, 'logs', 'max_dist.csv'), 'a') as f:
             writer = csv.writer(f)
             writer.writerow((tstep, max_dist))
-                
-
-    '''def save_loss(self, loss, policy_loss, value_loss, entropy_loss):
-        super(Model, self).save_loss(loss)
-        self.policy_losses.append(policy_loss)
-        self.value_losses.append(value_loss)
-        self.entropy_losses.append(entropy_loss)'''

networks/networks.py
@@ -21,8 +21,6 @@ class ActorCriticSMB(nn.Module):
         self.conv3 = init_(nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1))
         self.conv4 = init_(nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1))
 
-        #TODO: Left off here
-
         if use_gru:
             self.gru = nn.GRUCell(self.feature_size(input_shape), self.gru_size)
 
@@ -105,9 +103,8 @@ class ActorCriticSMB(nn.Module):
             return 1
 
 class ActorCriticAtari(nn.Module):
-    def __init__(self, input_shape, num_actions, conv_out=64, use_gru=False, gru_size=512):
+    def __init__(self, input_shape, num_actions, use_gru=False, gru_size=512):
         super(ActorCriticAtari, self).__init__()
-        self.conv_out = conv_out
         self.use_gru = use_gru
         self.gru_size = gru_size
 
@@ -117,7 +114,7 @@ class ActorCriticAtari(nn.Module):
 
         self.conv1 = init_(nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4))
         self.conv2 = init_(nn.Conv2d(32, 64, kernel_size=4, stride=2))
-        self.conv3 = init_(nn.Conv2d(64, self.conv_out, kernel_size=3, stride=1))
+        self.conv3 = init_(nn.Conv2d(64, 64, kernel_size=3, stride=1))
 
         if use_gru:
             self.fc1 = init_(nn.Linear(self.feature_size(input_shape), 512))

utils/hyperparameters.py
@@ -4,11 +4,11 @@ import math
 class PolicyConfig(object):
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.conv_out = 64
         self.reward_type = 'dense'
 
         #icm
-        self.icm_beta = 0.2
+        self.icm_loss_beta = 0.2
+        self.icm_prediction_beta = 0.01
         self.icm_lambda = 0.1
 
         #meta infor